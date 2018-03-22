d74201d03668127d4be95b53b1edb426287be709
Jason <jnumeroff@hotmail.com>
2018-03-22 18:51:33 -0400

    Refactor event handlers

--- index.js
+++ index.js
@@ -4,12 +4,11 @@ import Player from './lib/player';
 import Dealer from './lib/dealer';
 import Hand from './lib/hand';
 import Game from './lib/game';
-import { renderBankrolls, renderBet, playRound } from './lib/util.js';
+import { renderBankrolls, renderBet, playRound, startHand } from './lib/util.js';
 
 window.addEventListener('load', () => {
   const game = new Game;
   game.shoe.shuffle();
-  game.dealCards();
   renderBankrolls(game);
   playRound(game);
 
@@ -30,23 +29,23 @@ window.addEventListener('load', () => {
   });
 
   document.getElementById('bet25').addEventListener('click', () => {
-    game.player.bet = 25;
-    renderBet(game);
+    game.player.placeBet(game.player.currentHand, 25);
+    startHand(game);
   });
 
   document.getElementById('bet100').addEventListener('click', () => {
-    game.player.bet = 100;
-    renderBet(game);
+    game.player.placeBet(game.player.currentHand, 100);
+    startHand(game);
   });
 
   document.getElementById('bet500').addEventListener('click', () => {
-    game.player.bet = 500;
-    renderBet(game);
+    game.player.placeBet(game.player.currentHand, 500);
+    startHand(game);
   });
 
   document.getElementById('betAllIn').addEventListener('click', () => {
-    game.player.bet = game.player.bankroll;
-    renderBet(game);
+    game.player.placeBet(game.player.currentHand, game.player.bankroll);
+    startHand(game);
   });
 
 });
