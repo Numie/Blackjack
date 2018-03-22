4185b0ab60296615bedace51ff2fd09806cb7526
Jason <jnumeroff@hotmail.com>
2018-03-22 18:19:59 -0400

    Add bet setting

--- index.js
+++ index.js
@@ -4,13 +4,14 @@ import Player from './lib/player';
 import Dealer from './lib/dealer';
 import Hand from './lib/hand';
 import Game from './lib/game';
-import { renderBankrolls } from './lib/util.js';
+import { renderBankrolls, renderBet, playRound } from './lib/util.js';
 
 window.addEventListener('load', () => {
   const game = new Game;
   game.shoe.shuffle();
   game.dealCards();
   renderBankrolls(game);
+  playRound(game);
 
   document.getElementById('hitButton').addEventListener('click', () => {
     game.player.hit(game.shoe);
@@ -26,6 +27,26 @@ window.addEventListener('load', () => {
 
   document.getElementById('splitButton').addEventListener('click', () => {
     game.player.split();
+  });
+
+  document.getElementById('bet25').addEventListener('click', () => {
+    game.player.bet = 25;
+    renderBet(game);
+  });
+
+  document.getElementById('bet100').addEventListener('click', () => {
+    game.player.bet = 100;
+    renderBet(game);
+  });
+
+  document.getElementById('bet500').addEventListener('click', () => {
+    game.player.bet = 500;
+    renderBet(game);
+  });
+
+  document.getElementById('betAllIn').addEventListener('click', () => {
+    game.player.bet = game.player.bankroll;
+    renderBet(game);
   });
 
 });
